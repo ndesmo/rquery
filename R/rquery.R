e8e1f0ed33dd42c1d3753c18a7be0fe5d88c083a
UNIBET\nicdes <nicholas.desmond@unibet.com>
2017-10-03 12:06:47 +0100

    documentation for the rquery function itself

--- R/rquery.R
+++ R/rquery.R
@@ -1,3 +1,33 @@
+#' RQuery
+#'
+#' @description Query the database efficiently and cache data appropriately in subdirectories
+#'
+#' @param sql SQL query to run
+#' @param name name of query
+#' @param subs One row data frame whose column names are the substrings to replace, and whose values are the strings to replace them with.
+#' @param data.dir Query results directory. Stores the results of the SQL queries as RDS files.
+#' @param sql.dir SQL query directory. Stores all the SQL used. If subs were provided, it saves the query before the subs were inserted.
+#' @param subs.dir Subs directory. Stores all the individual subs for each query as a data frame.
+#' @param daily Boolean to indicate whether to force a rerun of the query on first execution each day.
+#' @param overwrite Boolean to indicate whether to force a rerun of the query.
+#'
+#' @return Data frame of the query results
+#'
+#' @export
+#' @examples
+#'
+#' sys.setenv(
+#'   'db_dsn' = 'mydb',
+#'   'db_username' = 'myuser',
+#'   'db_password' = 'password'
+#' )
+#'
+#' sql <- "select 1 as a from dual where 1 = _number_"
+#' subs <- data.frame('_number_' = 1)
+#'
+#' rquery(sql, 'myquery', subs)
+
+
 rquery <- function(sql = NULL, name, subs = NULL,
                    data.dir = 'data', sql.dir = 'sql', subs.dir = 'params',
                    daily = FALSE, overwrite = FALSE) {
